621e44e9976c9abdf6ef0fa5c54e75cc7164b3f8
Emanuela Boros <emanuela.boros@gmail.com>
2023-11-18 16:23:21 +0100

    Put Flamingo on Cuda

--- FLAMINGO/test_flam.py
+++ FLAMINGO/test_flam.py
@@ -2,9 +2,10 @@
 
 from open_flamingo import create_model_and_transforms
 from huggingface_hub import hf_hub_download
-import torch
 from PIL import Image
 import torch
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 
 model, image_processor, tokenizer = create_model_and_transforms(
     clip_vision_encoder_path="ViT-L-14",
@@ -20,39 +21,21 @@ checkpoint_path = hf_hub_download(
     "checkpoint.pt")
 model.load_state_dict(torch.load(checkpoint_path), strict=False)
 
+model.to(device)
 
-"""
-Step 1: Load image
-"""
+query_image = Image.open("../../data/test/comic/EXP-1956-07-25-a-i0105.jpg")
 
-query_image = Image.open("data/test/comic/EXP-1956-07-25-a-i0105.jpg")
-
-"""
-Step 2: Preprocessing images
-Details: For OpenFlamingo, we expect the image to be a torch tensor of shape
- batch_size x num_media x num_frames x channels x height x width.
- In this case batch_size = 1, num_media = 3, num_frames = 1,
- channels = 3, height = 224, width = 224.
-"""
 vision_x = [image_processor(query_image).unsqueeze(0)]
 vision_x = torch.cat(vision_x, dim=0)
 vision_x = vision_x.unsqueeze(1).unsqueeze(0)
 
-"""
-Step 3: Preprocessing text
-Details: In the text we expect an <image> special token to indicate where an image is.
- We also expect an <|endofchunk|> special token to indicate the end of the text
- portion associated with an image.
-"""
 tokenizer.padding_side = "left"  # For generation padding tokens should be on the left
 
 lang_x = tokenizer(
-    ["<image>Classify this image into one of the following classes: comic, drawing, game, graph, logo, map, photo, title"],
+    ["<image>Classify this image into one of the following classes: comic, drawing, game, graph, logo, map, photo, title. "],
     return_tensors="pt",
 )
-"""
-Step 4: Generate text
-"""
+
 generated_text = model.generate(
     vision_x=vision_x,
     lang_x=lang_x["input_ids"],
